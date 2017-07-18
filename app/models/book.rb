8f5619fe342bc61e2251f5a1fa9a5c67ed8da0e6
明野寛 <akenohiroshi@akenohiroshinoMacBook-Pro.local>
2017-07-18 17:11:01 +0900

    validates commit

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ApplicationRecord
 	has_many :comments, dependent: :destroy
+	validates :title, presence: true
+	validates :price, presence: true
+	validates :summary, presence: true
+	validates :evaluate, presence: true
 end
